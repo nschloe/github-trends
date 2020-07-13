66fc1a47cb711f9a1d6957229acd509043de1882
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-13 15:40:18 +0200

    hotware -> gh_stars

--- data/plot.py
+++ data/plot.py
@@ -17,7 +17,7 @@ for group_name, group in data.items():
     gh_stars.plot(filenames, cut=0.05)
     plt.title("Number of GitHub stars", fontsize=14)
 
-    # hotware.plot_per_day(filenames)
+    # gh_stars.plot_per_day(filenames)
     # plt.title("Daily number of GitHub stars", fontsize=14)
 
     plt.show()
